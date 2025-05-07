e9589030e55cb70e10980ed14a60a807d98dcc39
mrMcAko <alechacin08@gmail.com>
2025-05-07 17:30:43 -0600

    Add healtcheck to app.js

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const {port, appUri} = require('./config/constants');
+const healthCheckRoute = require('./routes/healtCheckRoute');
 
 connectDB();
 
 const app = express();
 app.use(express.json());
 
-app.use(appUri, (req, res) => {
-    res.send('API is running...');
-});
+app.use(appUri, healthCheckRoute); // Middleware para manejar rutas
 
 app.listen(port, console.log(`Server running on port ${port}${appUri}`));
