add15558e79960556a5ef66d5d9589bbb7250873
mrMcAko <alechacin08@gmail.com>
2025-05-17 00:15:44 -0600

    Se agrega la ruta de usuarios, a la api principal

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 const {port, appUri} = require('./config/constants');
 const healthCheckRoute = require('./routes/healtCheckRoute');
+const userRoutes = require('./routes/userRoutes');
 
 connectDB();
 
@@ -9,5 +10,6 @@ const app = express();
 app.use(express.json());
 
 app.use(appUri, healthCheckRoute); // Middleware para manejar rutas
+app.use (`${appUri}/users`, userRoutes)
 
 app.listen(port, console.log(`Server running on port ${port}${appUri}`));
