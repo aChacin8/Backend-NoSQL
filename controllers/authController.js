6420ac372244b6336f8a8d13f88fa2bbf285866d
mrMcAko <alechacin08@gmail.com>
2025-05-20 15:48:18 -0600

    Create the controller to register user with atentication JWT

---
+++ controllers/authController.js
@@ -0,0 +1,39 @@
+const ModelUser = require ('../models/Users');
+const jwt = require ('jsonwebtoken')
+const {keyToken} = require ('../config/constants')
+
+const generateToken = (id) => {
+    return jwt.sign({id}, keyToken, {expiresIn: '8d'});
+};
+
+const registerUser = async (req,res) => {
+    const {name, email, password} = req.body; 
+
+    const userExist = await ModelUser.findOne({email});
+
+    if (userExist){
+        res.status(400).json({message: 'User already exist'}) 
+    }
+
+    const user = await ModelUser.create({
+        name,
+        email,
+        password
+    });
+
+    if (user) {
+        res.status(201).json({
+            _id:user._id,
+            name: user.name,
+            email: user.email,
+            tokenAccess: generateToken(user._id)
+        })
+    } else {
+        res.status(400).json({message: error.message})
+    }
+}
+
+module.exports = {
+    generateToken,
+    registerUser
+}
