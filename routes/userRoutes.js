ea5712da01ff648f3677ea703c5c6b048dc3891a
mrMcAko <alechacin08@gmail.com>
2025-05-21 20:11:13 -0600

    feat: Create middleware to authorization endpoints with token

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require ('express');
 const {getAllUsers, createUsers} = require ('../controllers/userController.js');
 const { getAllPost, createPost } = require('../controllers/postController.js');
+const authMiddleware = require('../middlewares/authMiddleware.js');
 
 const userRoutes = express.Router();
 
-userRoutes.route('/').get(getAllUsers).post(createUsers)
-userRoutes.route('/posts').get(getAllPost).post(createPost)
+userRoutes.route('/').get( authMiddleware,getAllUsers).post(createUsers)
+userRoutes.route('/posts').get(authMiddleware ,getAllPost).post(createPost)
 
 module.exports = userRoutes; 
