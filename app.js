6b05a6b7886e8d75e3c67bc77cd3fb6476f05974
mrMcAko <alechacin08@gmail.com>
2025-06-03 23:24:43 -0600

    feat: Add the routes to files routes

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const connectDB = require('./config/db');
 const {port, appUri} = require('./config/constants');
 const healthCheckRoute = require('./routes/healtCheckRoute');
+const {generateErrors, celebrateErrorsHandler} = require ('./middlewares/errorMiddleware')
 const userRoutes = require('./routes/userRoutes');
 const authRoutes = require('./routes/authRoutes');
-const {generateErrors, celebrateErrorsHandler} = require ('./middlewares/errorMiddleware')
+const fileRoutes = require('./routes/fileRoutes');
 
 connectDB();
 
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(appUri, healthCheckRoute); // Middleware para manejar rutas
 app.use(`${appUri}/users`, userRoutes);
 app.use(`${appUri}/auth`, authRoutes);
+app.use(`${appUri}/files`, fileRoutes);
 
 app.use(celebrateErrorsHandler);
 app.use(generateErrors);
