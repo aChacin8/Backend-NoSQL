724a056f68e5901d01d1743de2b50d8e12700fcd
mrMcAko <alechacin08@gmail.com>
2025-06-03 23:23:39 -0600

    fix: Now the limit file is 5MB

--- middlewares/uploadMiddleware.js
+++ middlewares/uploadMiddleware.js
@@ -5,7 +5,7 @@ const storage = multer.memoryStorage(); // Almacena los archivos en memoria
 const upload = multer({
     storage,
     limits: {
-        fileSize: 5 * 1024 * 1024, // Limite de 5MB por archivo
+        fileSize: 1024 * 1024 * 5, // Limite de 5MB por archivo
     }, 
 });
 
